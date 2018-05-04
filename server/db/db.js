8b9d2748e3bdf2e323bbf95a5a1f70239e3616c5
Amal Sudama <cds.sudama@gmail.com>
2018-05-04 11:36:56 -0400

    get database url from env settings

--- server/db/db.js
+++ server/db/db.js
@@ -1,4 +1,9 @@
 const Sequelize = require('sequelize')
-const db = new Sequelize('postgres://localhost/optikos')
+const db = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://localhost/optikos',
+  {
+    logging: false
+  }
+)
 
 module.exports = db
