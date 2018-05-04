127dbfcbdd6f758ef743a96d12ccb9494dca5778
Amal Sudama <cds.sudama@gmail.com>
2018-05-04 11:57:40 -0400

    cheesy quote

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,11 @@
 const router = require('express').Router()
 module.exports = router
 
+router.use('/', (req, res, next) => {
+  res.send(`
+    <h1>You can’t use up creativity.<br />The more you use, the more you have.<br />-- Maya Angelou</h1>
+  `)
+})
+
 router.use('/api/projects', require('./api/project'))
 router.use('/api/tasks', require('./api/task'))
