a16dbd4727e9b4d738f46047265fd009958adb53
Amal Sudama <cds.sudama@gmail.com>
2018-05-09 10:39:07 -0400

    add routes for testing persistant layout

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+const path = require('path')
 const router = require('express').Router()
 module.exports = router
 
@@ -16,3 +18,34 @@ router.get('/', (req, res, next) => {
 
 router.use('/api/projects', require('./api/project'))
 router.use('/api/tasks', require('./api/task'))
+
+// for testing serialize/deserialize
+//
+const layoutFile = path.join(__dirname, 'sync-layout.json')
+
+router.post('/api/serialize', (req, res, next) => {
+  try {
+    const serializedLayout = JSON.stringify(req.body, undefined, 2)
+    console.log(
+      'POST Serialized Data',
+      JSON.stringify(serializedLayout, undefined, 2)
+    )
+    fs.writeFileSync(layoutFile, serializedLayout)
+    res.sendStatus(201)
+  } catch (error) {
+    console.log('POST Error', error)
+    next(error)
+  }
+})
+
+router.get('/api/serialize', (req, res, next) => {
+  try {
+    const serializedLayout = JSON.parse(fs.readFileSync(layoutFile).toString())
+    console.log('GET', serializedLayout)
+    console.log('layout:', serializedLayout)
+    res.send(serializedLayout)
+  } catch (error) {
+    console.log('GET Error', error)
+    next(error)
+  }
+})
